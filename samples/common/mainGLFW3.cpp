3d37255c99ed53e19eebc9ceb74cb26e8e9c6f40
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2018-08-12 23:31:14 -0400

    MSAA

--- samples/common/mainGLFW3.cpp
+++ samples/common/mainGLFW3.cpp
@@ -24,6 +24,7 @@ int main( int argc, char* args[] )
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SAMPLES, 4);
 
     GLFWwindow* window = glfwCreateWindow(app.getScreenWidth(),
                                           app.getScreenHeight(),
