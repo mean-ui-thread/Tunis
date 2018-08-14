ef3f2b2b08ec4a3aa5975bad7310e62145fdc585
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2018-08-13 21:03:02 -0400

    Paint refcount and recycling.

--- include/TunisPaint.h
+++ include/TunisPaint.h
@@ -19,87 +19,148 @@ public:
                                              Color innerColor,
                                              Color outerColor)
     {
+        float dx, dy, d;
+        const float large = 1e5;
+        // Calculate transform aligned to the line
+        dx = endX - startX;
+        dy = endY - startY;
+        d = sqrtf(dx*dx + dy*dy);
+        if (d > 0.0001f)
         {
-            float dx, dy, d;
-            const float large = 1e5;
-            // Calculate transform aligned to the line
-            dx = endX - startX;
-            dy = endY - startY;
-            d = sqrtf(dx*dx + dy*dy);
-            if (d > 0.0001f) {
-                dx /= d;
-                dy /= d;
-            } else {
-                dx = 0;
-                dy = 1;
-            }
-
-            Paint p(_soa.size());
-
-            _soa.push_back(
-                {dy, -dx, dx, dy, startX - dx*large, startY - dy*large}, // xform
-                {large, large + d*0.5f}, // extend
-                0.0f,                    // radius
-                std::max(1.0f, d),       // feather
-                innerColor,              // innerColor
-                outerColor,              // outerColor
-                0                        // image
-            );
-
-            return p;
+            dx /= d;
+            dy /= d;
         }
+        else
+        {
+            dx = 0;
+            dy = 1;
+        }
+
+        size_t id;
+
+        if (_available.size() > 0)
+        {
+            id = _available.back();
+            _available.pop_back();
+        }
+        else
+        {
+            id = _soa.size();
+            _soa.resize(_soa.size()+1);
+        }
+
+        _soa.get<_xform>(id) = {dy, -dx, dx, dy, startX - dx*large, startY - dy*large};
+        _soa.get<_extend>(id) = {large, large + d*0.5f};
+        _soa.get<_radius>(id) = 0.0f;
+        _soa.get<_feather>(id) = std::max(1.0f, d);
+        _soa.get<_innerColor>(id) = innerColor;
+        _soa.get<_outerColor>(id) = outerColor;
+        _soa.get<_image>(id) = 0;
+        _soa.get<_refCount>(id) = 1;
+
+        return Paint(id);
     }
 
     inline Paint(size_t id) : id(id)
     {
     }
 
-    inline float getXform(size_t index) const
+    inline Paint(const Paint &other) : id(other.id)
     {
-        return _soa.get<0>(id)[index];
+        ++_soa.get<_refCount>(id);
     }
 
-    inline float getExtend(size_t index) const
+    inline ~Paint()
     {
-        return _soa.get<1>(id)[index];
+        if (--_soa.get<_refCount>(id) == 0)
+        {
+            _available.push_back(id);
+        }
+    }
+
+    inline Paint &operator=(const Paint &other)
+    {
+        if (this != &other)
+        {
+            if (--_soa.get<_refCount>(id) == 0)
+            {
+                _available.push_back(id);
+            }
+
+            _soa.copy(other.id, id);
+            ++_soa.get<_refCount>(other.id);
+
+            id = other.id;
+        }
+
+        return *this;
+    }
+
+    inline const std::array<float, 6> &getXform() const
+    {
+        return _soa.get<_xform>(id);
+    }
+
+    inline const std::array<float, 2> &getExtend() const
+    {
+        return _soa.get<_extend>(id);
     }
 
     inline float getRadius() const
     {
-        return _soa.get<2>(id);
+        return _soa.get<_radius>(id);
     }
 
     inline float getFeather() const
     {
-        return _soa.get<3>(id);
+        return _soa.get<_feather>(id);
     }
 
     inline Color getInnerColor() const
     {
-        return _soa.get<4>(id);
+        return _soa.get<_innerColor>(id);
     }
 
     inline Color getOuterColor() const
     {
-        return _soa.get<5>(id);
+        return _soa.get<_outerColor>(id);
     }
 
     inline size_t getImage() const
     {
-        return _soa.get<6>(id);
+        return _soa.get<_image>(id);
+    }
+
+    inline int getRefCount() const
+    {
+        return _soa.get<_refCount>(id);
     }
 
 private:
 
+    enum {
+        _xform = 0,
+        _extend,
+        _radius,
+        _feather,
+        _innerColor,
+        _outerColor,
+        _image,
+        _refCount
+    };
+
     static SoA<
         std::array<float, 6>, // xform
         std::array<float, 2>, // extend
-        float, // radius
-        float, // feather
-        Color, // innerColor
-        Color, // outerColor
-        size_t // image
+        float,                // radius
+        float,                // feather
+        Color,                // innerColor
+        Color,                // outerColor
+        size_t,               // image
+        int                   // refCount;
     > _soa;
+
+    static std::vector<size_t> _available;
 
     size_t id;
 };
