f10d8fd0c042f02691b356789db6adb2cf922cce
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2018-08-24 20:59:31 -0400

    cmake control flag to enable/disable profiling

--- cmake/BuildOptions.cmake
+++ cmake/BuildOptions.cmake
@@ -7,3 +7,6 @@ option(TUNIS_BUILD_SAMPLES "Build samples" ON)
 set(TUNIS_SAMPLES_MAIN "SDL2" CACHE STRING "Main entry point implementation to usef for the samples")
 set_property(CACHE TUNIS_SAMPLES_MAIN PROPERTY STRINGS SDL2 GLFW3)
 
+option(TUNIS_PROFILING "Enable profiling using easy_profiling" ON)
+
+
