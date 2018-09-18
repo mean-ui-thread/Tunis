b74683fd3d00fea91359523ed385dd6693b7afb0
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2018-09-18 09:54:38 -0400

    re-enable MSAA, and shift every vertices by 0.333333 to improve contrast

--- samples/common/mainSDL2.cpp
+++ samples/common/mainSDL2.cpp
@@ -21,8 +21,8 @@ int main( int argc, char* args[] )
     SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 3 );
     SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 2 );
     SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
-//    SDL_GL_SetAttribute( SDL_GL_MULTISAMPLEBUFFERS, 1);
-//    SDL_GL_SetAttribute( SDL_GL_MULTISAMPLESAMPLES, 4);
+    SDL_GL_SetAttribute( SDL_GL_MULTISAMPLEBUFFERS, 1);
+    SDL_GL_SetAttribute( SDL_GL_MULTISAMPLESAMPLES, 4);
     SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute( SDL_GL_STENCIL_SIZE, 8);
 
